e8646f3e0a3f6ea9b6b9a3fbcb2d9bfa0e0985a8
Mahi-msg <maahika11gupta@gmail.com>
2026-07-29 15:09:11 +0530

    HelloWorld.jsx: Creating a component App.jsx: Importing the component and rendered it

--- Day/Day12/my-react-app/src/App.jsx
+++ Day/Day12/my-react-app/src/App.jsx
@@ -1,10 +1,11 @@
+import { HelloWorld } from 'SW_CSE_B1.4_Internship\Day\Day12\my-react-app\src\component\HelloWorld.jsx';
 function App() {
+  
   return (
-    <div className="App">
-      <h1>Hello, World!</h1>
-      <h3>Welcome to my React application.</h3>
-    </div>
-  );
+      <div>
+        <HelloWorld />
+      </div>
+  )
 }
 
-export default App;
+export default App
